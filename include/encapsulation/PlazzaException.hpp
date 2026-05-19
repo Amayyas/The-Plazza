b87718b2406e370d0fe2eef48c8461604adbe2c2
Yumi-alt <julie.vincent@epitech.eu>
2026-05-19 11:14:35 +0200

    feat: exception encapsulation

---
+++ include/encapsulation/PlazzaException.hpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2025
+** The-Plazza
+** File description:
+** Exception.hpp
+*/
+
+/// @file PlazzaException.hpp
+/// @brief PlazzaException class definition
+
+#ifndef PLAZZAEXCEPTION_HPP
+    #define PLAZZAEXCEPTION_HPP
+    #include <exception>
+    #include <string>
+
+/// @brief Namespace for the Plazza project
+namespace Plazza {
+    /// @brief Custom exception class for Plazza-related errors
+    class PlazzaException : public std::exception {
+        public:
+            /// @brief Constructs a PlazzaException object
+            /// @param message The error message describing the exception
+            PlazzaException(const std::string& message);
+
+            /// @brief Retrieves the error message associated with the exception
+            /// @return A C-style string representing the error message
+            [[nodiscard]] const char* what() const noexcept override;
+
+        private:
+            /// @brief The error message describing the exception
+            std::string _message;
+    };
+}  // namespace Plazza
+
+#endif
